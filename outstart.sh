0e00ac5a50b07fd32ea92cb81cd310ad183811cc
root <root@zsp.vmware.com>
2021-03-05 13:55:27 +0800

    deploy

--- outstart.sh
+++ outstart.sh
@@ -1,3 +1,4 @@
 
- ./mutong-eventer -frequency 10s -bufferwindows 50 -v 2  --source="kubernetes:https://zsp.vmware.com:6443?inClusterConfig=false&auth=/root/.kube/config" --sink="dingtalk:https://https://oapi.dingtalk.com/robot/send?access_token=9097668119105d47c33e86c67a9ac709496739a7f7dea3b6a7e46c4f88d29a6d&level=Warning&msg_type=markdown"
+ ./mutong-eventer -frequency 10s -bufferwindows 50 -v 2  --source="kubernetes:https://XXX:6443?inClusterConfig=false&auth=/root/.kube/config" --sink="dingtalk:https
+ XXX &level=Warning&msg_type=markdown"
 
